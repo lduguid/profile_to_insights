379c9b0691bc73a239499ee86cc6d170007cc9c5
Luke Duguid <sleep.king@gmail.com>
2025-03-28 11:39:28 -0700

    do each profle category to its own file. TODO convert captured 'block' output to tsv. order of curdata fields?

--- main.cpp
+++ main.cpp
@@ -8,14 +8,16 @@
 #include "defines.h"
 
 
-bool is_category_start(const char* start_category, const char* current)
+static bool is_category_start(const char* start_category, const char* current)
 {
-  if (strncmp(current, start_category, strlen(start_category)) == 0) return true;
+  if (strncmp(current, start_category, strlen(start_category)) == 0) 
+    return true;
+  
   return false;
 }
 
 
-bool is_category_end(const char* start_category, const char* current)
+static bool is_category_end(const char* start_category, const char* current)
 {
   for (int i = 0; i < sizeof(profile_categories) / sizeof(profile_categories[0]); i++)
   {
@@ -39,6 +41,7 @@ int main(char argc, char* argv[])
   }
 
   char buffer[1024] = { 0 };
+  char fputs_buffer[1024] = { 0 };
   bool found_category = false;
   int found_field_key = 0;
   int found_profile_info_name = 0;
@@ -46,6 +49,15 @@ int main(char argc, char* argv[])
 
   for (int ii = 0; ii < sizeof(profile_categories) / sizeof(profile_categories[0]); ii++)
   {
+    // open current category tsv file
+
+    FILE* categoryFile = fopen(profile_categories[ii].output_filename, "w");
+    if (categoryFile == NULL)
+    {
+      printf("Error: Could not open file %s\n", profile_categories[ii].output_filename);
+      return 1;
+    }
+
     for (int i = 0; i < sizeof(profile_info_list) / sizeof(profile_info_list[0]); i++)
     {
       if (!found_category && is_category_start(profile_categories[ii].name, profile_info_list[ii].name))
@@ -56,16 +68,19 @@ int main(char argc, char* argv[])
 
       if (found_category && !is_category_end(profile_categories[ii].name, profile_info_list[ii].name))
       {
-        printf("Category: %s\n", profile_categories[ii].name);
+        sprintf(fputs_buffer, "Category: %s\n", profile_categories[ii].name);
+        fputs(fputs_buffer, categoryFile);
 
-        // NEED to find in each '[FIELD]' block or blank cell.
         while (fgets(buffer, 1024, profileFile) != NULL)
         {
+          // NEED to find in each '[FIELD]' block or blank cell.
+
           if (strncmp(buffer, PROFILEFKEY, strlen(PROFILEFKEY)) == 0)
           {
             if (found_field_key != found_profile_info_name)
             {
-              printf("*blank*\n");
+              sprintf(fputs_buffer, "*blank*\n");
+              fputs(fputs_buffer, categoryFile);
               found_profile_info_name = found_field_key;
             }
 
@@ -73,31 +88,38 @@ int main(char argc, char* argv[])
           }
           else if (strncmp(buffer, profile_info_list[i].name, strlen(profile_info_list[i].name)) == 0)
           {
-            printf("%s", buffer);
+            sprintf(fputs_buffer, "%s", buffer);
+            fputs(fputs_buffer, categoryFile);
             found_profile_info_name++;
           }
           else if ((strncmp(buffer, PROFILEFNAME, strlen(PROFILEFNAME)) == 0))
           {
-            printf("%s", buffer);
+            sprintf(fputs_buffer, "%s", buffer);
+            fputs(fputs_buffer, categoryFile);
           }
           else if ((strncmp(buffer, PROFILEFTYPE, strlen(PROFILEFTYPE)) == 0))
           {
-            printf("%s", buffer);
+            sprintf(fputs_buffer, "%s", buffer);
+            fputs(fputs_buffer, categoryFile);
           }
 
           row_count++;
         }
 
+        // capture potential blank with last column field.
+
         if (found_field_key != found_profile_info_name)
         {
-          printf("*blank*\n");
+          sprintf(fputs_buffer, "*blank*\n");
+          fputs(fputs_buffer, categoryFile);
           found_profile_info_name = found_field_key;
         }
 
         found_field_key = 0;
         found_profile_info_name = 0;
 
-        printf("\n");
+        sprintf(fputs_buffer, "\n");
+        fputs(fputs_buffer, categoryFile);
         fseek(profileFile, 0, SEEK_SET);
       }
       else
@@ -105,6 +127,8 @@ int main(char argc, char* argv[])
         found_category = false;
       }
     }
+
+    fclose(categoryFile);
   }
 
   fclose(profileFile);
